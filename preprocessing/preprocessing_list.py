Fix wrong dimension in NormalizeSaturation

preprocessing/preprocessing_list.py
@@ -215,11 +215,11 @@ class NormalizeSaturation(BasePreprocessing):
 
         is_normalized: bool = False
 
-        reference_image_hsv = rgb2hsv(reference_image)
+        reference_image_hsv: np.ndarray = rgb2hsv(reference_image)
         reference_saturation: float = np.mean(reference_image_hsv[1])
 
-        image_hsv = rgb2hsv(image)
-        saturation: float = np.mean(image_hsv[1])
+        image_hsv: np.ndarray = rgb2hsv(image)
+        saturation: float = np.mean(image_hsv[:, :, 1])
 
         if abs(saturation - reference_saturation) / reference_saturation > 0.75:
             matched_hsv = match_histograms(image_hsv, reference_image_hsv, multichannel=True)