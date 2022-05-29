cd9ae2a6805a3d7a0408bcbc5fedcdbb47dd4956
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2022-05-29 22:23:30 +0200

    chore: wording
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- deploy.py
+++ deploy.py
@@ -14,11 +14,10 @@ if __name__ == "__main__":
     deploy_augmentation: bool = True
     deploy_preprocessing: bool = True
 
-    # If the instance has only 1 cpu core, augmentation and preprocessing
-    # each will consume half of the cpu, hence 0.5.
-    # Or if the instance has more than 2 cores, then augmentation uses 1 core,
-    # operating system uses 1 core,
-    # the rests will be used by preprocessing.
+    # If the instance has only 1 CPU core, augmentation and preprocessing each
+    # use half of the CPU, i.e. 0.5. If the instance has more than 2 cores,
+    # augmentation uses 1 CPU core, the operating system uses 1 CPU core,
+    # and the rest is used for preprocessing.
     if deploy_augmentation:
         AugmentationDeployment.options(
             route_prefix="/augmentation",
